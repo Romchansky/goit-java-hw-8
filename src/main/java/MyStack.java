b75d01dfc41a416581bfa2014428535b0a7c362f
Romchansky <romkapetrenko8@gmail.com>
2021-02-17 23:05:18 +0200

    Ex.4 add MyStack

--- src/main/java/MyStack.java
+++ src/main/java/MyStack.java
@@ -1,25 +1,83 @@
+import java.util.EmptyStackException;
 
+public class MyStack<E> {
 
-public class MyStack<E> extends MyLinkedList<E> {
+    static class MyNode<E> {
+        private E element;
+        private MyNode<E> next;
+
+        MyNode(E element) {
+            this.element = element;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public MyNode<E> getNext() {
+            return next;
+        }
+
+        public void setNext(MyNode<E> next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "Value is: " + element + " | " + next;
+        }
+    }
+
+    private MyNode<E> topElement = null;
+    int size = 0;
 
     public MyStack() {
     }
 
+    public void push(E element) {
+        MyNode<E> myNewNode = new MyNode<>(element);
+        myNewNode.setNext(topElement);
+        topElement = myNewNode;
+        size++;
+    }
+
     public E peek() {
-        return first.item;
+        if(isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return topElement.getElement();
     }
 
     public E pop() {
-        E retDelElement = first.item;
-        remove(0);
-        return retDelElement;
+       if(isEmpty()) {
+           throw new EmptyStackException();
+       }
+       MyNode <E> myNode = topElement.getNext();
+       size--;
+       return (E) myNode.getElement();
     }
 
-    public E push(E element) {
-        add(element);
-        return element;
+    public int size() {
+        return size;
     }
 
+    public void clear() {
+        topElement = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        return "" + topElement;
+    }
 
     public static void main(String[] args) {
 
@@ -35,5 +93,10 @@ public class MyStack<E> extends MyLinkedList<E> {
         System.out.println(stringMyStack.pop());
         System.out.println("-------------------------------------");
         System.out.println(stringMyStack);
+        System.out.println(stringMyStack.size());
+        System.out.println("-------------------------------------");
+        stringMyStack.clear();
+        System.out.println();
+
     }
 }
